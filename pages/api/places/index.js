3a93b439b00366c8ac361f633ee0da9170a5b343
Vanessa Di Bella <vanessa@di-bella.de>
2024-07-17 11:14:16 +0200

    add get request

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,16 +1,12 @@
-import Place from "@/db/models/Places";
+import Place from "@/db/models/Place";
 import dbConnect from "../../../db/dbConnect";
 
 export default async function handler(request, response) {
   await dbConnect();
-  const { id } = request.query;
   if (request.method === "GET") {
-    const place = await Place.findById(id);
-
-    if (!place) {
-      return response.status(404).json({ status: "Not Found" });
-    }
-
-    response.status(200).json(place);
+    const places = await Place.find();
+    return response.status(200).json(places);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
   }
 }
